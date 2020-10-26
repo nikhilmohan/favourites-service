bd2d194e15b1c67d32a4015639ee0327302dab73
nikhilmohan <nikmohan81@gmail.com>
2020-10-26 23:54:53 +0530

    initial commit

---
+++ src/main/java/com/nikhilm/hourglass/favouritesservice/FavouritesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nikhilm.hourglass.favouritesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouritesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavouritesServiceApplication.class, args);
+	}
+
+}
