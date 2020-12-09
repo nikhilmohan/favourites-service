384f26fb44ff636de962debb265a04e58b7f1a79
nikhilmohan <nikmohan81@gmail.com>
2020-12-09 23:08:30 +0530

    fixed gateway url

--- src/main/java/com/nikhilm/hourglass/favouritesservice/services/UserIntegrationService.java
+++ src/main/java/com/nikhilm/hourglass/favouritesservice/services/UserIntegrationService.java
@@ -1,6 +1,7 @@
 package com.nikhilm.hourglass.favouritesservice.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -11,10 +12,14 @@ public class UserIntegrationService {
     @Autowired
     WebClient webClient;
 
+    @Value("${service.url.gateway}")
+    private String serviceGatewayUrl;
+
+
     public Mono<Boolean> isUserLoggedIn(String userId)    {
 
         return webClient.
-                get().uri("http://localhost:9900/user-service/user/" + userId + "/status")
+                get().uri("http://" + serviceGatewayUrl + ":9900/user-service/user/" + userId + "/status")
                 .retrieve()
                 .bodyToMono(Boolean.class);
 
