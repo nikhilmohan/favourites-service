b8d6310a7edd277991d819292afcdb96b0ff8320
nikhilmohan <nikmohan81@gmail.com>
2020-12-13 10:42:17 +0530

    API doc updated

--- src/main/java/com/nikhilm/hourglass/favouritesservice/resource/FavouriteResource.java
+++ src/main/java/com/nikhilm/hourglass/favouritesservice/resource/FavouriteResource.java
@@ -7,6 +7,14 @@ import com.nikhilm.hourglass.favouritesservice.services.FavouriteMovieService;
 import com.nikhilm.hourglass.favouritesservice.services.FavouriteTriviaService;
 import com.nikhilm.hourglass.favouritesservice.services.SyncService;
 import com.nikhilm.hourglass.favouritesservice.services.TriviaMapper;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +37,14 @@ import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Favourites service API",
+                version = "1.0",
+                description = "API for managing movie/ trivia favourites for the user in hourglass application",
+                contact = @Contact( name = "Nikhil Mohan", email = "nikmohan81@gmail.com")
+        )
+)
 public class FavouriteResource {
 
     @Autowired
@@ -51,7 +67,15 @@ public class FavouriteResource {
         this.rcb = factory.create("favourites");
     }
 
-
+    @Operation(summary = "Fetch all favourite movies for the user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of movie favourites",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = FavouriteMoviesResponse.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content =  @Content ),
+            @ApiResponse(responseCode = "404", description = "Favourite list not found",
+                    content =  @Content )})
     @GetMapping("/favourites/user/{userId}/movies")
     public Mono<FavouriteMoviesResponse> getFavouriteMovies(@PathVariable("userId") String userId,
                                                     @RequestParam("ids") Optional<String> ids)   {
@@ -59,13 +83,30 @@ public class FavouriteResource {
 
     }
 
+    @Operation(summary = "Fetch all favourite trivia for the user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of trivia favourites",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = FavouriteTriviaResponse.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content =  @Content ),
+            @ApiResponse(responseCode = "404", description = "favourite list not found",
+                    content =  @Content )})
     @GetMapping("/favourites/user/{userId}/trivia")
     public Mono<FavouriteTriviaResponse> getFavouriteTrivia(@PathVariable("userId") String userId,
                                                             @RequestParam("terms") Optional<String> terms)   {
         return favouriteTriviaService.getFavouriteTrivia(userId, terms);
     }
 
-
+    @Operation(summary = "Change the favourite status of a movie (add/ remove)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success - Movie favourite status updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content =  @Content ),
+            @ApiResponse(responseCode = "404", description = "favourite resource for user not found",
+                    content =  @Content )})
     @PutMapping("/favourites/user/{userId}/movie")
     public Mono<ResponseEntity<Movie>> updateMovieAsFavourite(@PathVariable("userId") String userId, @RequestBody Movie movie,
                                                               @RequestHeader("user") String user) {
@@ -80,6 +121,10 @@ public class FavouriteResource {
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Create favourite list for the user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Favourite list initialized",
+                    content = { @Content })})
     @PostMapping("/favourites")
     public Mono<ResponseEntity<Object>> createFavouriteMovieList(@RequestHeader("user") String userId) {
 
@@ -91,6 +136,11 @@ public class FavouriteResource {
 
     }
 
+    @Operation(summary = "Fetch all favourites for the user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of movie and trivia favourites",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = FavouritesResponse.class)) })})
     @GetMapping("/favourites")
     public Mono<FavouritesResponse> getFavourites(@RequestHeader("user") String userId) {
         return Mono.zip(favouriteMovieService.getFavouriteMovies(userId, Optional.empty()),
@@ -104,6 +154,15 @@ public class FavouriteResource {
                                 return response;}));
     }
 
+    @Operation(summary = "Change the favourite status of a trivia (add/ remove)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success - Trivia favourite status updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Movie.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content =  @Content ),
+            @ApiResponse(responseCode = "404", description = "favourite resource for user not found",
+                    content =  @Content )})
     @PutMapping("/favourites/user/{userId}/trivia")
     public Mono<ResponseEntity<Trivia>> updateTriviaAsFavourite(@PathVariable("userId") String userId,
                                                                 @RequestBody TriviaDTO triviaDTO,
